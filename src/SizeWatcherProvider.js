1c7b177d5e3aef43b363d17a9a0c02d97d372347
Hayden <huanbin.c@gmail.com>
2023-01-23 10:54:09 -0800

    Default asynchronous observe handler (#2)
    Co-authored-by: Hayden Chen <hayden.chen@illumio.com>

--- src/SizeWatcherProvider.js
+++ src/SizeWatcherProvider.js
@@ -1,8 +1,15 @@
+import PropTypes from 'prop-types';
 import {Component, createContext, createElement} from 'react';
 
 export const SizeWatcherContext = createContext(null);
 
 export default class SizeWatcherProvider extends Component {
+  static propTypes = {
+    // Whether observer updates are synchronous or asynchronously debounced through requestAnimationFrame
+    // Asynchronous by default
+    sync: PropTypes.bool
+  }
+
   constructor(props, context) {
     super(props, context);
 
@@ -16,19 +23,33 @@ export default class SizeWatcherProvider extends Component {
   }
 
   componentDidMount() {
+    const {sync} = this.props;
     // Create resizeObservable that handles all children containers size change
     // One for all: https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/z6ienONUb5A/F5-VcUZtBAAJ
     this.resizeObservable = new ResizeObserver(entries => {
-      for (const {target} of entries) {
-        if (this.childrenContainers.has(target)) {
-          // Take offsetWidth/Height instead of entry.contentRect,
-          // to match width/height (if not changed) taken on SizeWatcher mount, to avoid calling findBreakpoint method there,
-          // since offsetWidth/Height return rounded values whereas contentRect returns fractions
-          const width = target.offsetWidth;
-          const height = target.offsetHeight;
+      const handler = () => {
+        for (const {target} of entries) {
+          if (this.childrenContainers.has(target)) {
+            // Take offsetWidth/Height instead of entry.contentRect,
+            // to match width/height (if not changed) taken on SizeWatcher mount, to avoid calling findBreakpoint method there,
+            // since offsetWidth/Height return rounded values whereas contentRect returns fractions
+            const width = target.offsetWidth;
+            const height = target.offsetHeight;
 
-          this.childrenContainers.get(target).handleSize({width, height});
+            this.childrenContainers.get(target).handleSize({width, height});
+          }
         }
+      };
+
+      if (sync) {
+        handler();
+      } else {
+        // Call the handler asynchronously to prevent "ResizeObserver loop limit exceeded" error
+        if (this.raf) {
+          window.cancelAnimationFrame(this.raf);
+        }
+
+        this.raf = window.requestAnimationFrame(handler);
       }
     });
 
@@ -37,6 +58,14 @@ export default class SizeWatcherProvider extends Component {
     this.childrenContainers.forEach(({dom}) => {
       this.resizeObservable.observe(dom);
     });
+  }
+
+  componentWillUnmount() {
+    if (this.raf) {
+      window.cancelAnimationFrame(this.raf);
+    }
+    this.resizeObservable?.disconnect();
+    this.childrenContainers.clear();
   }
 
   checkInChildContainer(dom, handleSize) {
